02d7324887089dd050a6283f5e64247983de8aeb
willian moura <44169840+ragnermoura@users.noreply.github.com>
2022-12-22 09:32:29 +0000

    Autenticação

---
+++ routes/nivel.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+router.get('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tb003_nivel',
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado })
+            }
+        )
+
+    })
+});
+
+router.post('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'INSERT INTO tb003_nivel (nome_nivel, descricao_nivel) VALUES (?,?)',
+            [
+                req.body.nivel,
+                req.body.descricao,
+               
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Nivel cadastrado com sucesso!',
+                    id_user: resultado.insertId
+                });
+            }
+        )
+    })
+
+});
+
+router.get('/:id_nivel', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tb003_nivel WHERE id_nivel = ?',
+            [req.params.id_user],
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado })
+            }
+        )
+
+    })
+});
+
+router.patch('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE tb003_nivel 
+            SET nome_nivel = ?, 
+            descricao_nivel = ?, 
+            WHERE id_nivel = ?`,
+            [
+                req.body.nivel,
+                req.body.descricao,
+                req.body.id_nivel,
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Dados do nivel alterados com sucesso!',
+                });
+            }
+        )
+    })
+});
+
+router.delete('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error }) }
+        conn.query(
+            'DELETE FROM tb003_nivel WHERE id_nivel = ?',
+            [
+                req.body.id_nivel
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+
+                return res.status(202).send({
+                    mensagem: 'Dados excluido com sucesso!'
+                });
+            }
+        )
+    })
+});
+
+module.exports = router;
---
+++ routes/usuarios.js
@@ -0,0 +1,204 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//SMS Service
+const accountSid = "ACe8580af80cb800ddc80c1b690394efec";
+const authToken = "e1cd64c1a30c475e0bad348f7ae24633";
+const client = require("twilio")(accountSid, authToken);
+
+
+router.get('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tb001_user',
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado })
+            }
+        )
+
+    })
+});
+
+router.get('/:id_user', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM tb001_user WHERE id_user = ?',
+            [req.params.id_user],
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado })
+            }
+        )
+
+    })
+});
+
+router.patch('/edit', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE tb001_user 
+            SET nome_user = ?, 
+            email_user = ?, 
+            senha_user = ?, 
+            telefone_user = ?, 
+            id_plano = ?,
+            id_nivel = ?,
+            WHERE id_user = ?`,
+            [
+                req.body.nome,
+                req.body.email,
+                req.body.senha,
+                req.body.telefone,
+                req.body.plano,
+                req.body.nivel,
+                req.body.id_user,
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    mensagem: 'Dados de usuário alterados com sucesso!',
+                });
+            }
+        )
+    })
+});
+
+router.delete('/delete', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error }) }
+        conn.query(
+            'DELETE FROM tb001_user WHERE id_user = ?',
+            [
+                req.body.id_user
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+
+                return res.status(202).send({
+                    mensagem: 'Usuário excluido com sucesso!'
+                });
+            }
+        )
+    })
+});
+
+
+//ROTAS DE SERVIÇO
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((err, conn) => {
+        if (err) { return res.status(500).send({ error: error }) }
+        const query = `SELECT * FROM tb001_user WHERE email_user = ?`;
+        conn.query(query, [req.body.email], (error, results, field) => {
+            conn.release();
+
+            if (err) { return res.status(500).send({ error: error }) }
+            if (results.length < 1) {
+                return res.status(401).send({
+                    mensagem: 'Falha na autenticação.'
+                })
+
+            }
+            bcrypt.compare(req.body.senha, results[0].senha_user, (err, result) => {
+                if (err) {
+                    return res.status(401).send({ mensagem: 'Falha na autenticação.' })
+
+                } if (result) {
+                    const token = jwt.sign({
+                        id_usuario: results[0].id_user,
+                        nome: results[0].nome_user,
+                        email: results[0].email_user,
+                        telefone: results[0].telefone_user,
+                        nivel: results[0].id_nivel
+                    }, process.env.JWT_KEY, {
+                        expiresIn: "6h"
+                    })
+
+                    return res.status(200).send({
+                        mensagem: 'Autenticado com sucesso!',
+                        token: token
+                    })
+                }
+                return res.status(401).send({ mensagem: 'Falha na autenticação.' })
+            })
+        })
+    })
+}),
+
+    router.post('/cadastro', (req, res, next) => {
+        let random = Math.random();
+        random = random * 6547 + 1;
+        random = Math.trunc(random);
+
+        mysql.getConnection((err, conn) => {
+            if (err) { return res.status(500).send({ error: error }) }
+            conn.query('SELECT * FROM tb001_user WHERE email_user = ?', [req.body.email], (error, result) => {
+                if (err) { return res.status(500).send({ error: error }) }
+                if (result.length > 0) {
+                    res.status(409).send({
+                        mensagem: 'Email já cadastrado, por favor insira um email diferente!'
+                    })
+                } else {
+                    bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+                        if (errBcrypt) {
+                            return res.status(500).send({ error: errBcrypt })
+                        }
+                        conn.query(
+                            'INSERT INTO tb001_user (nome_user, email_user, senha_user, telefone_user, id_plano, id_nivel) VALUES (?,?,?,?,?,?)',
+                            [req.body.nome, req.body.email, hash, req.body.telefone, req.body.plano, req.body.nivel],
+
+                            (error, result) => {
+                                conn.release();
+                                if (error) {
+                                    return res.status(500).send({
+                                        error: error,
+                                    });
+                                }
+
+                                const response = {
+                                    mensagem: 'Usuário cadastrado com sucesso',
+                                    usuarioCriado: {
+                                        id_user: result.insertId,
+                                        nome: req.body.nome,
+                                        email: req.body.email,
+                                        telefone: req.body.telefone,
+
+                                        request: {
+                                            tipo: 'GET',
+                                            descricao: 'Pesquisa um usuário',
+                                            url: 'http://localhost:3000/usuarios'
+                                        }
+                                    }
+                                }
+                                return res.status(202).send(response);
+
+                            });
+                    });
+                }
+            })
+
+
+            client.messages
+                .create({ body: 'Olá, seu código de verificação é: ' + random, from: "+13608034819", to: req.body.telefone })
+                .then(message => console.log(message.sid))
+
+        });
+
+    }),
+
+
+
+    module.exports = router;
